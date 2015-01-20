c3ceacdc18d447928804d0414f9dd63e13c59374
xaviervalens <xaviervalens@hotmail.com>
2015-01-20 20:15:21 +0100

    Update ProvesGit.java

--- src/provesgit/ProvesGit.java
+++ src/provesgit/ProvesGit.java
@@ -14,6 +14,6 @@ public class ProvesGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola mon");
     }
 }
